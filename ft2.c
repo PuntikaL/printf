5f1f4f5724b09d5a7ae3065fa0495920887d6a0d
Puntika Leepagorn <pleepago@z3t11c1.42bangkok.com>
2022-11-12 07:49:54 +0700

    update1

--- ft2.c
+++ ft2.c
@@ -1,19 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft2.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pleepago <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/11 21:31:27 by pleepago          #+#    #+#             */
+/*   Updated: 2022/11/11 21:57:56 by pleepago         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 
-int ft_printchar(char a)
+int ft_printchar(int a)
 {
-  write (1, &a, 1);
-  return (1);
+	write (1, &a, 1);
+	return (1);
 }
 
 int ft_printstr(char *str)
 {
-  int i;
-  
-  i = 0;
-  while (str[i])
-  {
-    ft_
-    i++;
-  }
+	int len;
+	int i;
+	
+	len = 0;
+	i = 0;
+	while (str[i])
+	{
+		len += ft_printchar(str[i]);
+		i++;
+	}
+}
+
+int	ft_printhex(unsigned int dec)
+{
+	if (dec > 9)
+		ft_printhex(dex / 16);
+	else
+		ft_p
+}
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	if (n == -2147483648)
+		ft_putstr("-2147483648");
+	else if (n < 0)
+	{
+		ft_putchar('-');
+		ft_putnbr(-n);
+	}
+	else if (n >= 10)
+	{
+		ft_putnbr(n / 10);
+		ft_putchar(n % 10 + '0');
+	}
+	else
+		ft_putchar(n + '0');
+}
+
+int ft_printpointer(int num)
+{
+	
 }
--- mainfunc.c
+++ mainfunc.c
@@ -1,27 +1,39 @@
-#include<stdarg.c>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mainfunc.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pleepago <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/11 21:33:48 by pleepago          #+#    #+#             */
+/*   Updated: 2022/11/11 21:51:41 by pleepago         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-int get_format(valist args, const char format)
+#include <stdarg.h>
+#include <unistd.h>
+
+int get_format(va_list args, const char format)
 {
-  int len;
+	int len;
   
-  len = 0;
-  if (format == 'c')
-    len += ft_printchar(va_args(args, int));
-  else if (format == 's')
-    len += ft_printchar(va_args(args, char *));
-  else if (format == 'p')
-    len += ft_printchar(va_args(args, char *));
-  else if (format == 'd')
-    len += ft_printchar(va_args(args, char *));
-  else if (format == 'u')
-    len += ft_printchar(va_args(args, char *)); 
-  else if (format == 'x')
-    len += ft_printchar(va_args(args, char *));
-  else if (format == '%')
-    len += ft_printchar(va_args(args, char *));
-  return (len);
+	len = 0;
+	if (format == 'c')
+		len += ft_printchar(va_args(args, int));
+	else if (format == 's')
+		len += ft_printchar(va_args(args, char *));
+	else if (format == 'p')
+		len += ft_printchar(va_args(args, unsigned long long));
+	else if (format == 'd')
+		len += ft_printchar(va_args(args, int));
+	else if (format == 'u')
+		len += ft_printchar(va_args(args, unsigned int)); 
+	else if (format == 'x')
+		len += ft_printchar(va_args(args, unsigned int));
+	else if (format == '%')
+		len += ft_printchar(va_args(args, char *));
+	return (len);
 }
-
 
 int ft_printf(const char *, ...)
 {
@@ -34,14 +46,14 @@ int ft_printf(const char *, ...)
   va_start(args, str);
   while (str[i])
   {
-    if (str[i] == '%')
-    {
-      i += 1;
-      print_length += get_format(args, str[i]);
-    }
-    else
-      print_length += ft_printchar(str[i])
-     i++;
+	if (str[i] == '%')
+	{
+	  i += 1;
+	  print_length += get_format(args, str[i]);
+	}
+	else
+	  print_length += ft_printchar(str[i])
+	 i++;
   }
   va_end(args);
   return (print_length);
